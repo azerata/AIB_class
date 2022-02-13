2966ebf43d1d248e320c475d698b750053938125
azerata <smedemarkhammer@gmail.com>
2022-02-13 15:36:19 +0100

    Project 1, tasks 1 & 2
    Tasks 1 & 2 completed, added explanatory text

--- project_01/project_1.py
+++ project_01/project_1.py
@@ -2,9 +2,21 @@
 from cmath import inf
 import numpy
 from fasta import fasta
-gap_cost = -5
+'''
+Dependant on packages: numpy & math
+Also needs CHR's fasta parser in same dir as this is run from 
+That and the input files ofcourse... 
+'''
+
 
 def cost(i:str, j:str)->int:
+    ''' 
+    Score matrix for match/mismatch
+    With translation from Nucleotide to idx
+    Takes to nucleotides as input (strings of length 1)
+    outputs the score for the given match/mismatch as int
+    '''
+
     score = numpy.array([[10, 2, 5, 2],
                          [2, 10, 2, 5],
                          [5, 2, 10, 2],
@@ -20,19 +32,31 @@ def cost(i:str, j:str)->int:
 #%%
 
 #%% build matrix
-seq2 =  "AATAAT"
-seq1 = "AAGG"
 
 
-i,j = len(seq1),len(seq2)
+def setup_matrix(seq1:str, seq2:str):
+    '''
+    Takes two strings as input
+    Strings should be two nucleotide sequences we wish to align
+    Returns a matrix of size seq1 X seq2
 
-def setup_matrix(seq1, seq2):
+    '''
+
     matrix = numpy.full([len(seq1)+1,len(seq2)+1], None)
     return matrix
 
 
 # %%
 def c(i, j, matrix, gap_cost):
+    ''' 
+    Input: 
+    Two integers i and j, each equivalent to the length of the corresponding nucleotide sequence
+    A matrix to work on (numpy array)  as output by setup_matrix function
+    A linear gap modifier (int)
+
+    Returns the optimal alignment cost of the two sequenses
+    '''
+
     if matrix[i,j] is None:
         v1, v2, v3, v4 = -inf, -inf, -inf, -inf
         if i > 0 and j > 0:
@@ -47,14 +71,58 @@ def c(i, j, matrix, gap_cost):
     return matrix[i,j]
 
 #%%
-def build_alignment(seq1, seq2, gap_score):
+def build_alignment(seq1:str, seq2:str, gap_score:int):
+    '''
+    Wrapper for the previous functions, call them in order to pipe inputs/outputs
+    Takes 3 inputs, 2 sequences, and a gap score
+    returns a tuple with the filled out matrix, and the an int the optimal alignment cost
+    '''
+
     matrix = setup_matrix(seq1, seq2)
     i, j = len(seq1), len(seq2)
     out = c(i, j, matrix, gap_score)
-    return out, matrix
+    return  (matrix, out)
+
+'''
+Area below for testing:
+'''
+
+#%% Test block 1 (Task 1)
+
+seq2 =  "AATAAT"
+seq1 = "AAGG"
+
+build_alignment(seq1, seq2, -5)
+
+# %% Test block 2 (fasta seqs, task 2)
+o = fasta("seq1.fasta")
+o2 = fasta("seq2.fasta")
+
+seq1 = o["Seq1"]
+seq2 = o2["Seq2"]
+i = build_alignment(seq1, seq2, -5)
+print(i[0])
+print(i[1])
+# %% Test sequences with answers (final = 27)
+
+seq1 = "TCCAGAGA"
+
+seq2 = "TCGAT"
+
+i = build_alignment(seq1, seq2, -5)
+print(i[0])
+print(i[1])
+# %% Test sequences with answers (final = 61)
 
 
-#%%
-print(build_alignment(seq1, seq2, -5))
+
+seq1 = "CGTGTCAAGTCT"
+
+seq2 = "ACGTCGTAGCTAGG"
+
+
+i = build_alignment(seq1, seq2, -5)
+print(i[0])
+print(i[1])
 
 # %%
