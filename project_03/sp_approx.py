85186b8fb58a493aeb685633c9c28bc551ddc19c
azerata <smedemarkhammer@gmail.com>
2022-03-22 17:02:39 +0100

    Update sp_approx.py

--- project_03/sp_approx.py
+++ project_03/sp_approx.py
@@ -5,7 +5,6 @@ from global_linear import optimal_alignment, optimal_score_matrix
 from fasta import fasta
 
 
-
 def outer_cost(path):
     '''
     Outer function to create the cost function from phylib format score matrix
@@ -17,7 +16,7 @@ def outer_cost(path):
     file = open(path)
     tmp = int(file.readline())
 
-    t = {}
+    t = {"n":0, "N":0, "r":0, "R":0, "s":0, "S":0}
     score = np.zeros((tmp,tmp))
     scores = [file.readline() for i in range(tmp)]
 
@@ -33,7 +32,7 @@ def outer_cost(path):
 
     return cost
 
-def get_sequences(file):
+def get_sequences(file, lim = 'a'):
     ''' 
     Takes variable "file" that is the path to the wanted fasta file
     uses fasta function by storm, to parse, and outputs dict, in which all sequences can be found.
@@ -41,38 +40,107 @@ def get_sequences(file):
     seq_dict = fasta(file)
     keys = [key for key in seq_dict.keys()]
     out = []
-    for key in keys:
+    for key in keys:    
         out.append(seq_dict[key].upper())
-    return out
+    if lim not in ['a', 'A']:
+        out = out[0:int(lim)]
+    return out, keys
 
 
 if __name__ == "__main__":
 
     cost = outer_cost(sys.argv[1])
 
-    sequences = get_sequences(sys.argv[2])
+    sequences, seq_names = get_sequences(sys.argv[2], sys.argv[4])
 
     gap = int(sys.argv[3])
+
+    sys.setrecursionlimit(10**5)
 
     T = optimal_score_matrix(sequences, gap, cost)
     
 
-    tmp = [sum(row) for row in T ]
+    tmp = [ sum(row) for row in T ]
     
     s = np.argmin(tmp)
 
     k = len(sequences)
 
-    M = []
-
     i = 0
 
-    seqs = optimal_alignment(sequences[s], sequences[i], gap, cost)
-    A = []
-    for n in zip(seqs[0], seqs[1]):
-        A.append([n[0], n[1]])
+    MA = []
+    while i < k:
+        M = MA
+        if i != s:
+            seqs = optimal_alignment(sequences[s], sequences[i], gap, cost)
+            A = []
+            for n in zip(seqs[0], seqs[1]):
+                A.append([n[0], n[1]])
 
-    print(A)
+            if  not MA:
+                MA = A
+            else:
+                MA = []
+                p = q = 0
+                while p < len(M) and q < len(A):
+                    if M[p][0] == '-' and A[q][0] == '-':
+                        M[p].append(A[q][1])
+                        MA.append(M[p])
+                        p += 1
+                        q += 1
+                    elif M[p][0] == '-' and A[q][0] != '-':
+                        M[p].append('-')
+                        MA.append(M[p])
+                        p += 1
+                    elif M[p][0] != '-' and A[q][0] == '-':
+                        tmp = ['-' for _ in range(len(M[p]))]
+                        tmp.append(A[q][1])
+                        MA.append(tmp)
+                        q += 1
+                    elif M[p][0] != '-' and A[q][0] != '-':
+                        M[p].append(A[q][1])
+                        MA.append(M[p])
+                        p += 1
+                        q += 1
+                
+                if p < len(M):
+                    while p < len(M):
+                        M[p].append('-') 
+                        MA.append(M[p])
+                        p += 1
+                #elif q < len(A):
+                #    while q < len(A):
+                #        tmp = ['-' for _ in range(len(M[0]))]
+                #        tmp[0] = A[q][0]
+                #        tmp[-1] = A[q][1]
+                #        MA.append(tmp)
+                #        q += 1
+        i +=1
+        
+
+    out =[ [] for _ in range(len(MA[0]))]
+
+    for i in range(len(out)):
+        for j in range(len(MA)):
+            out[i].append(MA[j][i])
+
+
+    printer = [ ''.join(seq) for seq in out]
+
+    seq_list = []
+    seq_list.append(seq_names[s])
+
+    for i in range(0, k):
+        if i != s:
+            seq_list.append(seq_names[i])
+
+    for n in zip(seq_list, printer):
+        print(f'>{n[0]}',f'{n[1].strip()}',sep='\n')
+
+
+
+
+
 
 
 
